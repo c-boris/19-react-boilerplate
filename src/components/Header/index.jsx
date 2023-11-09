705e2585493396f88c8b16ede39a766d1103af37
cboris <b.crochet@gmail.com>
2023-11-09 22:17:46 +0100

    optimization

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -19,7 +19,7 @@ const Header = () => {
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
-              className="h-8 w-auto"
+              className="h-8 w-10"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt=""
             />
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,3 +1,5 @@
+import Background from '../../assets/img/photo-bg.webp';
+
 const links = [
   { name: 'Open roles', href: '#' },
   { name: 'Internship program', href: '#' },
@@ -15,8 +17,8 @@ export default function About() {
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        alt=""
+        src={Background}
+        alt="Background image"
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
       />
       <div
